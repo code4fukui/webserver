1ebfbf5ce185ac4d06a12bf2902070a4b845aefa
Taisuke Fukuno <fukuno@jig.jp>
2022-09-28 11:05:43 +0900

    rename webserver → wsutil

--- demo1.js
+++ demo1.js
@@ -1,5 +1,5 @@
-//import { serveAPI } from "https://code4fukui.github.io/webserver/webserver.js";
-import { serveAPI } from "./webserver.js";
+//import { serveAPI } from "https://code4fukui.github.io/wsutil/wsutil.js";
+import { serveAPI } from "./wsutil.js";
 
 serveAPI("/api", async (param) => {
   return { response: "OK", param };
--- demo2.js
+++ demo2.js
@@ -1,5 +1,5 @@
-//import { serve, handleWeb, handleAPI } from "https://code4fukui.github.io/webserver/webserver.js";
-import { serve, handleWeb, handleAPI, rescors } from "./webserver.js";
+//import { serve, handleWeb, handleAPI } from "https://code4fukui.github.io/wsutil/wsutil.js";
+import { serve, handleWeb, handleAPI, rescors } from "./wsutil.js";
 
 serve(async (req, path) => {
   if (path == "/api") {
