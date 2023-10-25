2ae6de52d51f3345fe370185d1dbf1e12574df89
Taisuke Fukuno <fukuno@jig.jp>
2023-10-26 06:59:24 +0900

    support port arg option

--- demo4.js
+++ demo4.js
@@ -1,6 +1,8 @@
 //import { serveAPI } from "https://code4fukui.github.io/wsutil/wsutil.js";
 import { serveAPI } from "./wsutil.js";
 
+const port = Deno.args[0] || 8888;
+
 serveAPI("/api", async (param, req, path, conninfo) => {
   return { response: "OK", param };
-}, 8888);
+}, port);
