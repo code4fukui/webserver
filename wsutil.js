73904c718d20602475a8f63269bbd67adf854803
Taisuke Fukuno <fukuno@jig.jp>
2023-12-04 18:43:32 +0900

    use Deno.serve

--- wsutil.js
+++ wsutil.js
@@ -1,4 +1,3 @@
-import { serve as _serve } from "https://deno.land/std@0.157.0/http/server.ts";
 import { handleWeb } from "./handleWeb.js";
 import { handleAPI } from "./handleAPI.js";
 export { handleWeb, handleAPI };
@@ -6,7 +5,7 @@ export { resjson, rescbor, rescors } from "./resjson.js";
 import { program } from 'https://code4fukui.github.io/commander-es/index.js';
 
 program
-  .version("0.0.2")
+  .version("0.0.3")
   .argument("[port number]", "port number of the server socket")
   .option("--ipv4", "hostname become 0.0.0.0 instead of [::]")
   .parse();
@@ -15,12 +14,17 @@ const options = program.opts();
 
 const hostname = options.ipv4 ? "0.0.0.0" : "[::]";
 
-export const serve = (handle, defaultPort = 8000) => { // func(req, path, conninfo)
+export const serve = (handle, defaultPort = 8000, abortsignal = null) => { // func(req, path, conninfo)
   const port = parseInt((program.processedArgs ? program.processedArgs[0] : null) || defaultPort);
-  _serve(async (req, conninfo) => {
+  const options = {
+    port,
+    hostname,
+    signal: abortsignal,
+  };
+  Deno.serve(options, async (req, conninfo) => {
     const path = new URL(req.url).pathname;
     return await handle(req, path, conninfo);
-  }, { port, hostname });
+  });
 };
 
 export const serveAPI = (apipath, func, defaultPort = 8000) => { // func(param, req, path, conninfo)
